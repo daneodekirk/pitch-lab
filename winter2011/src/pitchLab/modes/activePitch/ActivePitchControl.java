aafa86ab6e3fe8d2dcea1df6cd661ebbb0a66629
daneodekirk@gmail.com <daneodekirk@gmail.com@37985fe6-53f9-88f0-7b7b-a8289a196f7d>
2011-03-12 23:35:13 +0000

    added documentation to the Active Pitch mode

--- winter2011/src/pitchLab/modes/activePitch/ActivePitchControl.java
+++ winter2011/src/pitchLab/modes/activePitch/ActivePitchControl.java
@@ -18,15 +18,14 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 	boolean practiceMode = false;
 	
 	int state = 0;
-	/*
-	 * SWITCH STATEMENT state MODES
-	 * case 0:	haven't started yet
-	 * case 1: 	Working/Busy/Random Tones Playing
-	 * case 2:	single note playing, user needs to drag bar to location, enter confirms choice
-	 * case 3:	means user can drag the bar! (ie: pressed in the correct location)
-	 * case 4:	dead case, before user can 'accidently press enter' -- must touch drag bar first
-	 */
 	
+    /**
+     * Sets the configuration for PitchLab such that Active Pitch mode is 
+     * enabled and its methods are utilized.
+     *
+     * @param pw The GUI piano window interface
+     * @param practice The boolean that determines if the user has selected practice mode
+     */
 	public ActivePitchControl(PianoWindow pw, boolean practice)
 	{
 		this.pw = pw;
@@ -43,6 +42,22 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 	}
 
 	
+    /**
+     * When the user triggeres a mouse down event on the piano window,
+     * the x position is stored into memory.
+     * If the event occures within a set margin, then the user has 
+     * successfully grabbed the line in the piano window in order to 
+     * change the frequency and position of the line.
+     *
+     * If the user has selected practice mode, the information window
+     * is updated with information calculated based on this mouse click 
+     * event.
+     *
+     * While the line is being dragged across the piano window the cursor
+     * changes to the Resize cursor to visually indicate dragging.
+     *
+     * @param e The mouse click event 
+     */
 	public void mousePressed(MouseEvent e)
 	{
         int x = e.getX();   // Save the x coord of the click	
@@ -69,6 +84,11 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 		} 
 	}
 	
+    /**
+     * If the user is within the bounds of grabbing the line to interact with 
+     * the piano window, then the cursor will change to a Hand indicating
+     * the ability to grab the line.
+     */
 	public void adjustInteractiveCursor(MouseEvent e)
 	{
 		if (state >= 2 && state != 3)
@@ -82,21 +102,37 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 			pw.setCursor(new Cursor(Cursor.WAIT_CURSOR));
 	}
 	
+    /**
+     * Mouse move event
+     */
 	public void mouseMoved(MouseEvent e)
 	{
 		adjustInteractiveCursor(e);
 	}
 	
+    /**
+     * Mouse enter event
+     */
 	public void mouseEntered(MouseEvent e)
 	{
 		adjustInteractiveCursor(e);
 	}
 	
+    /**
+     * Mouse leave event
+     */
 	public void mouseExited(MouseEvent e)
 	{
 		adjustInteractiveCursor(e);
 	}
 	
+    /**
+     * Mouse drag event which updates the x-coordinate of the line in memory
+     * and by how much the line has been dragged.
+     *
+     * If practice mode is turned on, updates to the information window are
+     * made accordingly.
+     */
 	public void mouseDragged(MouseEvent e)
 	{
 	    if (state == 3) 
@@ -117,14 +153,15 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
         		pw.instruct.setCurrAnsSelected(Calculations.freqToProperNote(pw.contSine.getFrequency()));		
             }
             apm.blendNotes_ap(xp, this.practiceMode);
-            
             pw.repaint();
-            
         }
 		
 	}
 	
 	
+    /**
+     * Mouse up event. Adjusts cursor accordingly.
+     */
 	public void mouseReleased(MouseEvent e)
 	{
 		if (state == 3)
@@ -137,14 +174,25 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 		}
 	}
 	
+    /**
+     * Key press event which switches based on the current state, the states are indicated
+     * as follows:
+     *
+     * case 0:	The PitchLab test hasnt started yet
+     * case 1: 	Working/Busy/Random Tones Playing
+     * case 2:	Single note playing, user needs to drag bar to location, enter confirms choice
+     * case 3:	Means user can drag the bar! (ie: pressed in the correct location)
+     * case 4:	Dead case, before user can 'accidently press enter' -- must touch drag bar first
+     *
+     */
 	public void keyPressed(KeyEvent e)
 	{
 		if (e.getKeyCode() == KeyEvent.VK_ENTER)
 		{
 			switch (state)
 			{
-				case 0: //thing has not yet started
-					state = 1;  // set busy
+				case 0: 
+					state = 1;
 					pw.setCursor(new Cursor(Cursor.WAIT_CURSOR));
 					cycleAndPlay();
 					state = 4;
@@ -155,7 +203,7 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 					break;
 					
 				case 2:
-					state = 1;  //set busy
+					state = 1;
 					pw.setCursor(new Cursor(Cursor.WAIT_CURSOR));
 					
 					long now = System.currentTimeMillis();
@@ -194,14 +242,14 @@ public class ActivePitchControl extends pitchLab.pianoWindow.PianoWindowListener
 					else
 						pw.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
 					break;
-			}//end switch
-		}//end if
-	}//end method
+			}
+		}
+	}
 	
 	
-	//
-	//	CYCLE AND PLAY
-	//
+    /**
+     * Cycle and play the random notes heard at the beginning of each test.
+     */
 	private void cycleAndPlay()
 	{
 		if (this.practiceMode)
